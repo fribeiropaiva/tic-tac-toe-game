96c37b0d36a3064fbf0bd8f5b5920a99d43c2b74
Ribeiro <filipepaiva240@gmail.com>
2019-11-28 21:04:21 -0300

    Updates

---
+++ precache-manifest.56f6cc0a080e5b9829a49f531460038c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562ddd252c5c9cdced368bbebe829b38",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "767233917d1b4cac440b",
+    "url": "/tic-tac-toe-game/static/css/main.ae068d9f.chunk.css"
+  },
+  {
+    "revision": "86d3cb0e9930a4ca94b6",
+    "url": "/tic-tac-toe-game/static/js/2.3dc0bebe.chunk.js"
+  },
+  {
+    "revision": "767233917d1b4cac440b",
+    "url": "/tic-tac-toe-game/static/js/main.dc7a4673.chunk.js"
+  },
+  {
+    "revision": "436c19f47f292ac3ba3c",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.6fd12759.js"
+  }
+]);
